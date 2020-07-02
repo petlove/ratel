84ea056ba48c678a30300cc19458fc5e06f617a6
Danilo Velozo <velozo.dan@gmail.com>
2020-07-02 18:25:02 -0300

    Fix validator spec

--- app/validators/valid_text_validator.rb
+++ app/validators/valid_text_validator.rb
@@ -3,7 +3,6 @@
 class ValidTextValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     bad_word = Ratel::Validator.invalid?(value.to_s)
-
     return unless bad_word
 
     record.errors.add(attribute, "#{bad_word} é inválido(a)")

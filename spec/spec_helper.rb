1ccf37c878737ddb4c7e041db3fa322f206799cd
Laura Sirianni <laura.melo@petlove.com.br>
2025-03-27 14:51:18 -0300

    Fix: Require logger

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 require 'bundler/setup'
 require 'support/configs/simple_cov_config'
+require 'rails'
+require 'ratel'
+
 SimpleCovConfig.configure
 
 require File.expand_path('../spec/dummy/config/environment.rb', __dir__)
-ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '../../../spec/dummy'
-
-require 'rspec/rails'
-require 'ratel'
+ENV['RAILS_ROOT'] ||= File.expand_path('../../spec/dummy', __dir__)
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
